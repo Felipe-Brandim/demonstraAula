cd48e566b022aa11d5fe8a5d1d672164b3319d87
Felipe Brandim <felipe.brandim@gmail.com>
2025-03-12 14:17:02 -0300

    Deixando o `database.js` mais robusto com o uso de error handling.

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,25 @@
 import { Client } from "pg";
 
-async function query(queryObject){
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-    });
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
 
-    await client.connect();
-    const result= await client.query(queryObject);
-    await client.end(); 
+  try {
+    const result = await client.query(queryObject);
     return result;
-}   
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
 
 export default {
-    query:query,
-}; //exporta o objeto query
+  query: query,
+};
